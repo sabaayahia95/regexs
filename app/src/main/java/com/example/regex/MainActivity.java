8bb6d0ebd65738b941a57fa0b4d3529ca3cbfc4e
sabaayahia95 <sabaayahia4@gmail.com>
2020-10-28 19:48:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/regex/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.regex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView mmTextview ;
+    String re ;
+    String text ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mmTextview = findViewById (R.id.t2);
+        String re = "[a-zA-Z0-9]";
+        Pattern pt= Pattern.compile(re);
+        Matcher mt = pt.matcher( mmTextview.getText());
+
+        if(mt.matches()){
+            Toast.makeText(this,"true" ,Toast.LENGTH_LONG).show() ;
+        }
+        else {
+        Toast.makeText(this,"false",Toast.LENGTH_LONG).show() ;
+        }
+
+}}
